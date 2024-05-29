ac79c0e82e3af1380d0ab1980c1de4298831018f
zarhell <johan.zubieta@gmail.com>
2024-05-29 00:19:20 -0500

    feat: creating project initial structure

---
+++ src/main/java/com/patronesdisenno/patrones_disennopoc/PatronesDisennopocApplication.java
@@ -0,0 +1,13 @@
+package com.patronesdisenno.patrones_disennopoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronesDisennopocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronesDisennopocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patronesdisenno/patrones_disennopoc/PatronesDisennopocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patronesdisenno.patrones_disennopoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatronesDisennopocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
