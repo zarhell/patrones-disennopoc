c1d4b7c074e947b3e340a693048dcd892c72f145
zarhell <johan.zubieta@gmail.com>
2024-05-29 02:24:46 -0500

    feat: actualziando the application main class

--- src/main/java/com/patronesdisenno/patrones_disennopoc/PatronesDisennopocApplication.java
+++ src/main/java/com/patronesdisenno/patrones_disennopoc/PatronesDisennopocApplication.java
@@ -1,8 +1,13 @@
 package com.patronesdisenno.patrones_disennopoc;
 
+import com.patronesdisenno.patrones_disennopoc.application.ports.out.NotificationService;
+import com.patronesdisenno.patrones_disennopoc.domain.services.NotificationServiceImpl;
+import com.patronesdisenno.patrones_disennopoc.infrastructure.persistence.EquipmentRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.patronesdisenno.patrones_disennopoc.application.ports.out.EquipmentRepository;
 @SpringBootApplication
 public class PatronesDisennopocApplication {
 
@@ -10,4 +15,13 @@ public class PatronesDisennopocApplication {
 		SpringApplication.run(PatronesDisennopocApplication.class, args);
 	}
 
+	@Bean
+	public EquipmentRepository equipmentRepository() {
+		return new EquipmentRepositoryImpl();
+	}
+
+	@Bean
+	public NotificationService notificationService() {
+		return new NotificationServiceImpl();
+	}
 }
